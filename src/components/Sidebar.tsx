e6bcfbf78e7278508e3a2eee053f1bfec75383ed
luccasecco <luccasecco@hotmail.com>
2022-10-12 14:56:04 -0300

    change css

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -38,7 +38,7 @@ export function Sidebar(){
   }
 
   return(
-    <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
+    <aside className="w-[348px] h-screen overflow-y-scroll bg-gray-700 p-6 border-l border-gray-600">
       <div className="flex justify-between font-bold text-2xl pb-6 mb-6 border-b border-gray-500">
         <Button 
           color="yellow"
