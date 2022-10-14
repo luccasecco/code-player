58efd26cf10369ec4237e850a7eb52f021447a91
luccasecco <luccasecco@hotmail.com>
2022-10-14 10:26:44 -0300

    change css button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps{
 export function Button({children, onOptionChange, color, className}: ButtonProps){
   return(
     <button 
-      className={`w-28 h-8 ${color === 'yellow' ? 'bg-yellow-300' : 'bg-blue-300'} text-stone-900 text-base ${color === 'yellow' ? 'hover:bg-yellow-500' : 'hover:bg-blue-500'} transition-colors ${className}`}
+      className={`w-28 h-8 rounded ${color === 'yellow' ? 'bg-yellow-300' : 'bg-blue-300'} text-stone-900 text-base ${color === 'yellow' ? 'hover:bg-yellow-500' : 'hover:bg-blue-500'} transition-colors ${className}`}
       onClick={onOptionChange}
     >
       {children}
