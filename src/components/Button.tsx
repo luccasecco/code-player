46967409e0d0fc30d5201c8c7e705acef1cb2b63
luccasecco <luccasecco@hotmail.com>
2022-10-15 12:41:32 -0300

    fiz css button not focus opacity

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps{
 export function Button({children, onOptionChange, color, className}: ButtonProps){
   return(
     <button 
-      className={`w-28 h-8 rounded ${color === 'yellow' ? 'bg-yellow-300' : color === 'blue' ? 'bg-blue-300' : color === 'green' ? 'bg-green-300' : 'bg-violet-400'} text-stone-900 text-base  transition-all opacity-50 focus:opacity-100 ${className}`}
+      className={`w-28 h-8 rounded ${color === 'yellow' ? 'bg-yellow-300' : color === 'blue' ? 'bg-blue-300' : color === 'green' ? 'bg-green-300' : 'bg-violet-400'} text-stone-900 text-base  transition-all opacity-70 focus:opacity-100 ${className}`}
       onClick={onOptionChange}
     >
       {children}
