874b680f364df264ec1adb4f76ff1b4938a0021a
luccasecco <luccasecco@hotmail.com>
2022-10-15 12:24:01 -0300

    change button and button focus css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps{
 export function Button({children, onOptionChange, color, className}: ButtonProps){
   return(
     <button 
-      className={`w-28 h-8 rounded ${color === 'yellow' ? 'bg-yellow-300' : color === 'blue' ? 'bg-blue-300' : color === 'green' ? 'bg-green-300' : 'bg-violet-400'} text-stone-900 text-base ${color === 'yellow' ? 'hover:bg-yellow-500' : color === 'blue' ? 'hover:bg-blue-500' : color === 'green' ? 'hover:bg-green-400' : 'hover:bg-violet-500'} transition-colors focus:border-2 border-pink-600 ${className}`}
+      className={`w-28 h-8 rounded ${color === 'yellow' ? 'bg-yellow-300' : color === 'blue' ? 'bg-blue-300' : color === 'green' ? 'bg-green-300' : 'bg-violet-400'} text-stone-900 text-base  transition-all opacity-50 focus:opacity-100 ${className}`}
       onClick={onOptionChange}
     >
       {children}
